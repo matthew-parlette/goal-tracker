077a9f76698e980af5e9931a26d2ef2d5715402a
Matt Parlette <matthew.parlette@gmail.com>
2013-03-25 16:54:45 -0400

    Implement mongodb as a backend and display current goals.
    This commit adds a display for the goals in the collection titled 'goals', and also implements a handler to add a new goal. There still needs to be a way to add one through the web interface.

--- goals.py
+++ goals.py
@@ -1,10 +1,17 @@
 import tornado.ioloop
 import tornado.web
 import os.path
+import logging
+import pymongo
+import datetime
 
 from tornado.options import define,options
 
 define("port", default=8000, help="run on the given port", type=int)
+
+#Globals
+db = None
+coll = None
 
 class Application(tornado.web.Application):
   def __init__(self):
@@ -21,14 +28,34 @@ class Application(tornado.web.Application):
 
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
+    #goals is a cursor object
+    goals = coll.find()
+    print "found %s goals" % str(goals.count())
+    
     self.render(
       "index.html",
       page_title = "Here's a page",
-      header_text = "Web server is running",
-      footer_text = "Great job",
+      header_text = "Goal List",
+      footer_text = "footer",
+      goals = goals,
     )
 
+class NewGoalHandler(tornado.web.RequestHandler):
+  def post(self):
+    goal = self.request.body
+    insert_id = coll.insert({
+      'goal': goal,
+      'created': datetime.datetime.utcnow(),
+      'done': 0,
+      'deleted': 0,
+    })
+    print "Successfully added %s" % (str(insert_id))
+
 if __name__ == "__main__":
+  db = pymongo.Connection()['test']
+  #print "collections: %s" % str(db.collection_names())
+  coll = db['test']['goals']
+  
   app = Application()
   app.listen(options.port)
   tornado.ioloop.IOLoop.instance().start()
